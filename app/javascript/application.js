dcc6ffb98822a2d7e241b48b7a114325ac155a34
DavidAshburn <oneinchwebbing@gmail.com>
2022-07-05 15:23:37 -0700

    integrating js w/ inline function dec in about.html

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+console.log("Hello, World!")
+
+function callOut() {
+	console.log("About page")
+}
